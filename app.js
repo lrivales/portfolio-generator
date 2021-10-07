c06213a45b463f217246c5f5508b96f1fa46b88e
Lawrence Rivales <lrivales@gmail.com>
2021-10-06 23:55:37 -0400

    askBCS for portfolioData reference error

--- app.js
+++ app.js
@@ -1,13 +1,84 @@
 const inquirer = require('inquirer');
-inquirer
-    .prompt([
+
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself.'
         }
-    ])
-    .then(answers => console.log(answers));
+    ]);
+};
+
+const promptProject = portfolioData => {
+    // if there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type:'input',
+            name: 'description',
+            message: 'Provide a description of the project (required).'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? Check all that apply.',
+            choices: ['Javascript','HTML','CSS','ES6','jQuery','Bootstrap','Node']
+        },
+        {
+            input: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project (required).'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: 'false'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: 'false'
+        }
+    ]);
+};
+
+promptUser()
+    .then(promptProject)
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
 
